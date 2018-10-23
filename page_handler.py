7aa7b12572370d1a5c8cc4d754837057ccd20329
Jihoon Lee <lunapocket@gmail.com>
2018-10-23 14:49:14 +0900

    page_handler 기본 골격

---
+++ page_handler.py
@@ -0,0 +1,87 @@
+from urllib.parse import urlparse, urlunparse, urljoin 
+
+import requests
+import bs4
+
+from autolog import autolog, call_log_class, call_log_class_soft, blogger
+
+@call_log_class
+class Browser():
+	default_port = 80
+
+	def __init__(self, UA = '', default_port = 80):
+		self.UA = UA
+		self.default_port = default_port
+
+	def load_page(self, url = ''):
+		pass
+		# self.page = page(url)
+
+@call_log_class
+class RequestHandler(Browser):
+
+	def __init__(url = None):
+		if(url) self.addr = url
+		self.default_headers = {'user-agent': self.UA}
+
+	@property
+	def addr(self):
+		try:
+			return self._addr
+		except AttributeError:
+			return None
+
+	@addr.setter
+	def addr(self, url ='', port = default_port):
+		parsed = urlparse(url)
+		if(len(parsed.scheme) == 0 and len(parsed.netloc) == 0 and len(parsed.path) > 0):
+			try:
+				parsed = urlparse('http://' + url)
+			except:
+				raise ValueError('somethings wrong with url')
+
+		if(parsed.scheme != 'http'):
+			raise ValueError('inappropriate url using other protocol')
+
+		if(self.addr is not None and parsed.path[0] == '/'): #/path 의 경우 현재 내가 접속한 곳을 기준으로 바꾼다.
+				new_path = parsed.path
+				parsed = urlparse(self.addr)
+				parsed = parsed._replace(path = new_path)
+
+		try: #url 자체가 port 번호를 가지고 있는지 파악, port번호가 없으면 port 번호를 받아서 넘겨준다.(기본 80)
+			port = parsed.netloc.split(':')[1]
+		except IndexError:
+			parsed = parsed._replace(netloc = parsed.netloc + ":" + str(port))
+
+
+		self._addr = urlunparse(parsed)
+
+
+	def do_get(self, URL = addr, params = '', headers = default_headers):
+		self.addr = URL #URL을 형식에 맞게 갱신
+		r = requests.get(self.addr, params = params, headers = headers)
+		r.raise_for_status()
+
+		return r
+
+
+	def do_post(self, URL = addr, data = '', headers = default_headrs):
+		self.addr = URL
+		r = requests.post(self.addr, data = data, headers = headers)
+		r.raise_for_status()
+
+		return r
+
+	def do_put(self, URL = addr, data = '', headers = default_headrs):
+		self.addr = URL;
+		r = requests.put(self.addr, data = data, headers = headers)
+		r.raise_for_status()
+
+		return r
+
+	def parseHTML():
+		pass
+
+if __name__ == '__main__':
+	b = Browser()
+	b.addr = 'www.naver.com/asdf/asdf.png'
